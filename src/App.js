66d26811b8392f7506dbb7c8197402184aec3a1c
warmjuly <evertsmeldris@gmail.com>
2022-10-05 03:16:20 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route, HashRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import Deals from "./routes/Deals";
 import Booking from "./routes/Booking";
@@ -8,14 +8,12 @@ import Contact from "./routes/Contact";
 function App() {
 	return (
 		<div className="App">
-			<HashRouter>
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/deals" element={<Deals />} />
-					<Route path="/booking" element={<Booking />} />
-					<Route path="/contact" element={<Contact />} />
-				</Routes>
-			</HashRouter>
+			<Routes>
+				<Route exact path="/" element={<Home />} />
+				<Route path="/deals" element={<Deals />} />
+				<Route path="/booking" element={<Booking />} />
+				<Route path="/contact" element={<Contact />} />
+			</Routes>
 		</div>
 	);
 }
