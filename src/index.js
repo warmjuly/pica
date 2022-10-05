c3e438c78cce9a14a7f969443b6f2697e119d977
warmjuly <evertsmeldris@gmail.com>
2022-10-05 03:10:28 +0200

    .

--- src/index.js
+++ src/index.js
@@ -7,6 +7,11 @@ import { Route, HashRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<HashRouter basename="process.env.PUBLIC_URL">
-		<Route exact path="/" component={App} />
+		<Routes>
+			<Route path="/" element={<Home />} />
+			<Route path="/deals" element={<Deals />} />
+			<Route path="/booking" element={<Booking />} />
+			<Route path="/contact" element={<Contact />} />
+		</Routes>
 	</HashRouter>
 );
