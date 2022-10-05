b3adb819120dc1d41b1b709d82477a588a259f4f
warmjuly <evertsmeldris@gmail.com>
2022-10-05 03:11:18 +0200

    ..

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { Route, HashRouter } from "react-router-dom";
+import Home from "./routes/Home";
+import Deals from "./routes/Deals";
+import Booking from "./routes/Booking";
+import Contact from "./routes/Contact";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
